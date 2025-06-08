85b3fe41daa82f5b4b1c99b27423842c2b85b153
Philopater Mansour <philopater.mansour@gmail.com>
2025-06-08 12:25:00 +0000

    feat(helm): deploy monitoring stack and Jenkins via Helm

---
+++ Terraform/Modules/Helm/variables.tf
@@ -0,0 +1,15 @@
+variable "ebs_csi_irsa_role_arn" {
+  type = string
+}
+variable "cluster_endpoint" {
+  type = string
+}
+variable "cluster_ca_certificate" {
+  type = string
+}
+variable "cluster_name" {
+  type = string
+}
+variable "region" {
+  type = string
+}
