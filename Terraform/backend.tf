fd1214b777e382251fb0f9176b6b8c0387e7d86d
Philopater Mansour <philopater.mansour@gmail.com>
2025-06-09 17:00:00 +0000

    chore: add main Terraform wiring, Jenkinsfile, and guestbook Helm chart

---
+++ Terraform/backend.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+  profile= "admin"
+}
+terraform {
+  backend "s3" {
+    bucket       = "eks-monitor-terraform-bucket"
+    key          = "terraform/state"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
